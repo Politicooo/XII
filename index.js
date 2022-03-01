3faab4fc9bebb285dde4fde56ad9aa6008793f96
Politicooo <97833277+Politicooo@users.noreply.github.com>
2022-03-01 21:41:18 +0100

    Upload

---
+++ index.js
@@ -0,0 +1,202 @@
+const Discord = require("discord.js")
+const client = new Discord.Client(
+    { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_INTEGRATIONS"] })
+
+client.login(process.env.token)
+
+client.on ("ready", () =>  {
+    console.log ("Il bot è online!")
+
+    var server = client.guilds.cache.get("924024543096487977")
+    server.commands.create({
+        name: "kick",
+        description: "Espellere un utente",
+        options: [
+            {
+                name: "user",
+                description: "L'utente da espellere",
+                type: "USER",
+                required: true
+            },
+            {
+                name: "reason",
+                description: "Motivazione",
+                type: "STRING",
+                required: true
+            }
+        ]
+    })
+})
+client.on('ready', () => {
+    client.user.setActivity('Ⅻ 🌓', { type: 'PLAYING' });
+    client.user.setActivity("Ⅻ 🌓", {
+    });
+    client.user.setStatus('idle') 
+})
+client.on("messageCreate", message => {
+    if (message.content.startsWith("x?userinfo")) {
+        if (message.content == "!userinfo") {
+            var utente = message.member;
+        }
+        else {
+            var utente = message.mentions.members.first();
+        }
+        if (!utente) {
+            return message.channel.send("Non ho trovato questo utente")
+        }
+        var elencoPermessi = "";
+        if (utente.permissions.has("ADMINISTRATOR")) {
+            elencoPermessi = "👑 ADMINISTRATOR";
+        }
+        else {
+            var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "ADMINISTRATOR", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS_AND_STICKERS", "USE_APPLICATION_COMMANDS", "REQUEST_TO_SPEAK", "MANAGE_THREADS", "CREATE_PUBLIC_THREADS", "CREATE_PRIVATE_THREADS", "USE_EXTERNAL_STICKERS", "SEND_MESSAGES_IN_THREADS", "START_EMBEDDED_ACTIVITIES"]
+            for (var i = 0; i < permessi.length; i++)
+                if (utente.permissions.has(permessi[i]))
+                    elencoPermessi += `- ${permessi[i]}\r`
+        }
+        var embed = new Discord.MessageEmbed()
+            .setTitle(utente.user.tag)
+            .setDescription("Tutte le info di questo utente")
+            .setThumbnail(utente.user.displayAvatarURL())
+            .addField("User id", utente.user.id, true)
+            .addField("Status", utente.presence ? utente.presence.status : "offline", true)
+            .addField("Is a bot?", utente.user.bot ? "Yes" : "No", true)
+            .addField("Account created", utente.user.createdAt.toDateString(), true)
+            .addField("Joined this server", utente.joinedAt.toDateString(), true)
+            .addField("Permissions", elencoPermessi, false)
+            .addField("Roles", utente.roles.cache.map(ruolo => ruolo.name).join("\r"), false)
+        message.channel.send({ embeds: [embed] })
+    }
+})
+client.on("messageCreate", message => {
+    if (message.content == "x?serverinfo") {
+        var server = message.guild;
+        var embed = new Discord.MessageEmbed()
+            .setTitle(server.name)
+            .setDescription("Tutte le info su questo server")
+            .setThumbnail(server.iconURL())
+            .addField("Owner", client.users.cache.get(server.ownerId).username, true)
+            .addField("Server id", server.id, true)
+            .addField("Members", server.memberCount.toString(), false)
+            .addField("Channels", server.channels.cache.size.toString(), false)
+            .addField("Server created", server.createdAt.toDateString(), true)
+            .addField("Boost level", "Level " + (server.premiumTier != "NONE" ? server.premiumTier : 0) + " (Boost: " + server.premiumSubscriptionCount + ")", true)
+        message.channel.send({ embeds: [embed] })
+    }
+})
+client.on("messageCreate", message => {
+    if (message.content.startsWith("x?clear")) {
+        if (!message.member.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non ho il permesso');
+        }
+        var count = parseInt(message.content.split(/\s+/)[1]);
+        if (!count) {
+            return message.channel.send("**Inserisci un numero valido!**")
+        }
+        if (count > 100) {
+            return message.channel.send("**Non puoi cancellare più di 100 messaggi!**")
+        }
+        message.channel.bulkDelete(count, true)
+        message.channel.send(count + " messaggi eliminati!").then(msg => {
+            setTimeout(() => msg.delete(), 5000)
+        })
+    }
+})
+client.on("messageCreate", message => {
+    if (message.content.startsWith("x?ban")) {
+        var utente = message.mentions.members.first();
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!utente) {
+            return message.channel.send('Non hai menzionato nessun utente');
+        }
+        if (!utente.bannable) {
+            return message.channel.send('Io non ho il permesso');
+        }
+        utente.ban()
+            .then(() => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle(`${utente.user.username} bannato`)
+                    .setDescription(`**Utente bannato da ${message.author.toString()}**`)
+
+                message.channel.send({ embeds: [embed] })
+            })
+    }
+})
+client.on("messageCreate", async message => {
+    if (message.content.startsWith("x?unban")) {
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+
+        var args = message.content.split(/\s+/);
+        var idUtente = args[1]
+
+        if (!idUtente) {
+            return message.channel.send("Non hai scritto l'id di nessun utente");
+        }
+
+        message.guild.members.unban(idUtente)
+            .then(() => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle("Utente sbannato")
+                    .setDescription("Questo utente è stato sbannato")
+
+                message.channel.send({ embeds: [embed] })
+            })
+            .catch(() => { message.channel.send("Utente non valido o non bannato") })
+    }
+})
+client.on("messageCreate", message => {
+    if (message.content.startsWith("x?kick")) {
+        var utente = message.mentions.members.first();
+        if (!message.member.permissions.has('KICK_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!utente) {
+            return message.channel.send('Non hai menzionato nessun utente');
+        }
+        if (!utente.kickable) {
+            return message.channel.send('Io non ho il permesso');
+        }
+        utente.kick()
+            .then(() => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle(`${utente.user.username} kickato`)
+                    .setDescription(`**Utente kickato da ${message.author.toString()}**`)
+
+                message.channel.send({ embeds: [embed] })
+            })
+    }
+})
+client.on("interactionCreate", interaction => {
+    if (!interaction.isCommand()) return
+
+    if(interaction.commandName == "kick") {
+        if(!interaction.member.permissions.has("KICK_MEMBERS")){
+            return interaction.reply({ content: "**Non hai il permesso!**", ephemeral: true })
+        }
+
+        var utente = interaction.options.getUser("user")
+        var reason = interaction.options.getString("reason")
+
+        var member = interaction.guild.members.cache.get(utente.id)
+        if(!member?.kickable){
+            return interaction.reply({ content: "**Non posso kickare questo utente!**"})
+        }
+
+        member.kick()
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Utente kickato")
+            .setThumbnail(utente.displayAvatarURL())
+            .addField("User", utente.toString())
+            .addField("Reason", reason)
+
+        interaction.reply({ embeds: [embed] })    
+    }
+}) 
